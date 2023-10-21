86b58b6d2fc47d4964d3970133f23f1098ad8005
Michel Schaffers <michel.schaffers@gmail.com>
2023-10-21 15:44:28 +0200

    Et ca marche

--- web/html/index.js
+++ web/html/index.js
@@ -2,9 +2,30 @@ console.log('Michel test site: script loaded but changed');
 
 
 function handleClick(e) {
-  console.log('button clicked');
-}
-
+    e.preventDefault();
+    const now = new Date();
+    console.log(`${now.toISOString()}: onclick`);
+  }
+  function handleTouchStart(e) {
+    const now = new Date();
+    const targets = e.targetTouches;
+    console.log(`${now.toISOString()}: ontouchstart`);
+    if ( targets ){
+        for ( target of targets){
+            console.log(`.. target ${target.target.localName}, ${target.target.id}`)
+        }
+        if ( target.target.localName==='input'){
+            target.target.checked = !target.target.checked;
+        }
+    }
+  }
+  function handleTouchEnd(e) {
+    const now = new Date();
+    console.log(`${now.toISOString()}: ontouchend`);
+  }
+    
 const button = document.querySelector("#michelbutton");
-const container = document.querySelector("#container");
-button.addEventListener("click", handleClick);
+const container = document.querySelector("#container1");
+container.addEventListener("click", handleClick);
+container.addEventListener("touchstart", handleTouchStart);
+container.addEventListener("touchend", handleTouchEnd);
